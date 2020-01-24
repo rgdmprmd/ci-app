c28d6c1c956952f5460dde9d68df1dadc8a375d8
Rangga Dimas Permadi <37428450+rgdmprmd@users.noreply.github.com>
2020-01-24 16:55:27 +0700

    add table new

---
+++ tb_team_spv_person_gogo.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v8.61 
+MySQL - 5.5.5-10.1.40-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `tb_team_spv_person_gogo` (
+	`no_int` double ,
+	`no_int_team` double ,
+	`id_spv` varchar (90),
+	`fl_active` tinyint (1),
+	`tag_asm` tinyint (1)
+); 
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('1','10714','mega_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('2','10714','asa_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('3','10714','vivi_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('4','10714','wulan_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('8','10735','asa_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('7','10735','mega_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('9','10735','vivi_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('10','10735','wulan_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('11','10759','mega_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('12','10759','asa_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('13','10759','vivi_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('14','10759','wulan_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('15','10764','mega_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('16','10764','vivi_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('17','10764','wulan_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('18','10777','mega_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('19','10777','vivi_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('20','10777','wulan_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('21','10789','mega_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('22','10789','vivi_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('23','10811','vivi_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('24','10811','mega_gogo_alsut','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('25','10798','E-COMMERCE (SPV)','1','1');
+insert into `tb_team_spv_person_gogo` (`no_int`, `no_int_team`, `id_spv`, `fl_active`, `tag_asm`) values('26','10802','E-COMMERCE (SPV)','1','1');
